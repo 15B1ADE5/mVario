b658f22f96672a763c17d9371ee24efcd073d308
Imperatar <imperatar@protonmail.com>
2021-07-13 13:20:42 +0200

    separated stucture for project

---
+++ vario/make_variables.mk
@@ -0,0 +1,15 @@
+DEVICE     = atmega328p
+CLOCK      = 8000000
+CC         = avr-gcc
+CFLAGS     = -c -std=gnu99 -Os -Wall -ffunction-sections -fdata-sections -mmcu=$(DEVICE) -DF_CPU=$(CLOCK)
+LINK       = -Os -mmcu=$(DEVICE) -ffunction-sections -fdata-sections -Wl,--gc-sections
+
+PROGRAMMER = arduino
+BAUD       = 57600
+PORT       = /dev/ttyUSB0
+
+BIN_DIR    = $(TOP_DIR)/bin
+OBJ_DIR    = $(BIN_DIR)/obj
+
+
+AVRDUDE = avrdude -F -v -p $(DEVICE) -c $(PROGRAMMER) -P $(PORT) -b $(BAUD) -D 

d4b83693cd471fce98ca4382291742541e3731bb
Imperatar <imperatar@protonmail.com>
2021-08-01 00:50:07 +0200

    main Makefile update: display memory usage

--- vario/make_variables.mk
+++ vario/make_variables.mk
@@ -1,3 +1,5 @@
+NAME       = vario
+
 DEVICE     = atmega328p
 F_CPU      = 8000000
 # I2C_MODE: TWI/I2C (ATmega328p is TWI compatible)
@@ -11,6 +13,10 @@ PORT       = /dev/ttyUSB0
 BIN_DIR    = $(TOP_DIR)/bin
 OBJ_DIR    = $(BIN_DIR)/obj
 
+ELF_FILE   = $(BIN_DIR)/$(NAME).elf
+HEX_FILE   = $(BIN_DIR)/$(NAME).hex
+
+
 CC         = avr-gcc
 CCPP       = avr-g++
 
@@ -20,4 +26,7 @@ ASFLAGS    = -c -Os -mmcu=$(DEVICE) -DF_CPU=$(F_CPU)UL -x assembler-with-cpp -Wa
 
 LDFLAGS    = -Os -mmcu=$(DEVICE) -ffunction-sections -fdata-sections -Wl,--gc-sections
 
-AVRDUDE = avrdude -F -v -p $(DEVICE) -c $(PROGRAMMER) -P $(PORT) -b $(FLASH_BAUD) -D 
+#AVRDUDE = avrdude -F -v -p $(DEVICE) -c $(PROGRAMMER) -P $(PORT) -b $(FLASH_BAUD) -D 
+AVRDUDE    = avrdude -v -p $(DEVICE) -c $(PROGRAMMER) -P $(PORT) -b $(FLASH_BAUD) -D
+
+AVRSIZE    = avr-size --format=avr --mcu=$(DEVICE) -C -x
